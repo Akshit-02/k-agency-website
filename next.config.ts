9ca0f41bfb357c0b3acc08bdf696b042f1c1f17b
akshit <akshitmurarka2002@gmail.com>
2026-09-06 11:26:26 +0530

    added redirect

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async redirects() {
+    return [
+      {
+        source: "/creators",
+        destination: "/for-creators",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
